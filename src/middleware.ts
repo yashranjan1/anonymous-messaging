8830bbc6e7c4ede28a38f73ef4a38e23b40d5ace
Yash Ranjan <yash.ranjan25@gmail.com>
2024-12-16 12:16:32 +0530

    middleware debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
     const url = new URL(request.nextUrl);
 
-    console.log(token)
+    console.log("cookies" + request.cookies)
 
     if (token && 
         (
